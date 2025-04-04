c53688f3eda2ed23d3b766fc265f8c8078de861a
Rahul <86316159+rahul17081998@users.noreply.github.com>
2025-04-04 18:42:50 +0530

    expose custom jmx-metrics into prometheus without jmx-exporter

--- src/main/java/com/rahul/journal_app/jmx/MBeanMetrics.java
+++ src/main/java/com/rahul/journal_app/jmx/MBeanMetrics.java
@@ -1,6 +1,8 @@
 package com.rahul.journal_app.jmx;
 
+import io.micrometer.core.instrument.Gauge;
 import io.micrometer.core.instrument.MeterRegistry;
+import io.micrometer.core.instrument.Tags;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 
@@ -13,44 +15,39 @@ import java.lang.management.ManagementFactory;
 public class MBeanMetrics {
 
     public MBeanMetrics(MeterRegistry registry) throws Exception {
-        /*
-        TodoListMBean mBean = new TodoList();
-        ObjectName objectName = new ObjectName("rahul.example:type=TodoList, name=TodoListMBean");
-        mBeanServer.registerMBean(mBean, objectName);
-        return mBean;
-        getTodoSize
-        -javaagent:/Users/rahulkumar/jmx_prometheus_javaagent-1.2.0.jar=1234:/Users/rahulkumar/Documents/mongoDb-springboot/journalApp/config/jmx_exporter_config.yml
-        */
+
         MBeanServer mBeanServer = ManagementFactory.getPlatformMBeanServer();
-//        ObjectName objectName = new ObjectName("rahul.example:type=TodoList");
-//
-//
-//        registry.gauge("rahul_example_todoSize", this, m -> {
-//            try {
-//                int x= (int) mBeanServer.getAttribute(objectName, "TodoSize");
-//                return x;
-//            } catch (Exception e) {
-//                log.info("Error to get jmx metric data: Exception: {}", e.getMessage());
-//                return -1; // Default if error occurs
-//            }
-//        });
 
 
-        ObjectName memoryObject = new ObjectName("java.lang:type=Memory");
-        registry.gauge("jvm_x_heap_memory_usage", this, m -> {
+        ObjectName objectName = new ObjectName("rahul.example:type=TodoList");
+        registry.gauge("rahul_example_todoSize", this, m -> {
             try {
-                CompositeData heapMemoryUsage = (CompositeData) mBeanServer.getAttribute(memoryObject, "HeapMemoryUsage");
-                long usedHeapMemory = (long) heapMemoryUsage.get("used");
-                return usedHeapMemory;
+                int x= (int) mBeanServer.getAttribute(objectName, "TodoSize");
+                return x;
             } catch (Exception e) {
                 log.info("Error to get jmx metric data: Exception: {}", e.getMessage());
                 return -1; // Default if error occurs
             }
         });
-//        CompositeData heapMemoryUsage = (CompositeData) mBeanServer.getAttribute(memoryObject, "HeapMemoryUsage");
-//        long usedHeapMemory = (long) heapMemoryUsage.get("used");  // Extracting "used" memory
-//        log.info("UsedHeapMemory: {}", usedHeapMemory);
-//        registry.gauge("jvm_x_heap_memory_usage", usedHeapMemory);
 
+
+        ObjectName memoryObject = new ObjectName("java.lang:type=Memory");
+        CompositeData heapMemory = (CompositeData) mBeanServer.getAttribute(memoryObject, "HeapMemoryUsage");
+        long usedHeapMemory = (long) heapMemory.get("used");
+        long maxHeapMemory = (long)heapMemory.get("max");
+        long committedHeapMemory = (long)heapMemory.get("committed");
+        long initHeapMemory = (long)heapMemory.get("init");
+
+        Gauge.builder("jvm_x_heap_memory_usage", ()-> 0)
+                .description("Custom metric to monitor JVM heap memory usage from JMX")
+                .tags(Tags.of("type", "heap")
+                        .and("source", "jmx MBean")
+                        .and("env", "local")
+                        .and("used", String.valueOf(usedHeapMemory))
+                        .and("max", String.valueOf(maxHeapMemory))
+                        .and("committed", String.valueOf(committedHeapMemory))
+                        .and("init", String.valueOf(initHeapMemory))
+                )
+                .register(registry);
     }
 }
